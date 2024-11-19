8e97e305cee85aa24848bc60d89fb425cb3723b7
SamratTheProgrammer <166220572+SamratTheProgrammer@users.noreply.github.com>
2024-11-20 00:43:11 +0530

    Delete OnlineExamination directory

--- OnlineExamination/src/main/webapp/Admin/AdminJs/Script.js
+++
@@ -1,8 +0,0 @@
-let modal1 = document.getElementById('profile-container');
-   
-       function openprofile() {
-           modal1.classList.add('show');
-         function closeprofile() {
-           modal1.classList.remove('show');
-       }
-       }
